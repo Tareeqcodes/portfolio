e283851fc4dc1162163f1ceeb401da863c3d5323
Devtareeq <101841038+Tareeqcodes@users.noreply.github.com>
2024-08-02 17:34:03 +0100

    create index.html
    my portfolio

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const boxes = document.querySelectorAll('.box');
+
+  window.addEventListener('scroll', checkBoxes);
+
+  function checkBoxes() {
+      const triggerBottom = window.innerHeight / 5 * 4;
+
+      boxes.forEach(box => {
+          const boxTop = box.getBoundingClientRect().top;
+
+          if (boxTop < triggerBottom) {
+              box.classList.add('show');
+          } else {
+              box.classList.remove('show');
+          }
+      });
+  }
+
+  checkBoxes(); // Initial check
+  var acc = document.getElementsByClassName('accordion');
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle('active');
+  
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.nextElementSibling;
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+      } else {
+        panel.style.display = 'block';
+      }
+    });
+  }
+  
+  ()=> {
+    console.log('hello word !');
+  }
+
+});
+
